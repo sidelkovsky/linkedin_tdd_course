72b65f214616068947ff73466848e05d32c7b679
sidelkovsky <sidelkovsky@tuta.io>
2023-07-22 03:54:07 +0400

    All basic class functionality with tests

--- SupermarketCheckoutKata/Checkout.py
+++ SupermarketCheckoutKata/Checkout.py
@@ -1,12 +1,50 @@
 class Checkout:
+    class Discount:
+        def __init__(self, nbrOfItems, price):
+            self.nbrOfItems = nbrOfItems
+            self.price = price
 
-    def addItemPrice(self, name, price):
-        pass
+    def __init__(self):
+        self.prices = {}
+        self.discounts = {}
+        self.items = {}
 
-    def addItem(self, name):
-        pass
+    def addItemPrice(self, item, price):
+        self.prices[item] = price
+
+    def addItem(self, item):
+        if item in self.items:
+            self.items[item] += 1
+        else:
+            self.items[item] = 1
 
     def calculateTotal(self):
-        return 1
+        total = 0
+        for item, cnt in self.items.items():
+            total += self.calculateItemTotal(item, cnt)
+        return total
 
+    def calculateItemTotal(self, item, cnt):
+        total = 0
+        if item in self.discounts:
+            discount = self.discounts[item]
+            if cnt >= discount.nbrOfItems:
+                total += self.calculateItemDiscountedTotal(item, cnt, discount)
+            else:
+                total += self.prices[item] * cnt
 
+        else:
+            total += self.prices[item] * cnt
+        return total
+
+    def calculateItemDiscountedTotal(self, item, cnt, discount):
+        total = 0
+        nbrOfDiscounts = cnt / discount.nbrOfItems
+        total += nbrOfDiscounts * discount.price
+        remaining = cnt % discount.nbrOfItems
+        total += remaining * self.prices[item]
+        return total
+
+    def addDiscount(self, item, nbrOfItems, price):
+        discount = self.Discount(nbrOfItems, price)
+        self.discounts[item] = discount
